7ec765ccaf4f077bb751b60f97086b7de9edd70f
kagarise <3255385205@qq.com>
2021-05-13 12:17:28 +0800

    api

--- api/v1/models/aip/__init__.py
+++ api/v1/models/aip/__init__.py
@@ -5,6 +5,7 @@ import requests
 
 from config import baidu_config
 from utils.cos import cos_config, upload_local_file
+from utils.logger import logger
 
 access_token_api = 'https://openapi.baidu.com/oauth/2.0/token'
 aip_api = 'http://tsn.baidu.com/text2audio'
@@ -25,8 +26,10 @@ def get_aip(API_KEY, SECRET_KEY, tex, cuid='kagarise', ctp=1, lan='zh', spd=5, p
     tok = get_access_token(client_id=API_KEY, client_secret=SECRET_KEY)
     try:
         access_token = tok['refresh_token']
+        logger.success(f'获取token成功')
     except:
         access_token = None
+        logger.error(f'获取token失败')
     print(access_token)
     params = {
         'tex': tex,
@@ -48,6 +51,8 @@ def get_aip(API_KEY, SECRET_KEY, tex, cuid='kagarise', ctp=1, lan='zh', spd=5, p
         upload_local_file(bucket_name=cos_config['BucketName'], local_file_path=file_path, file_path=file_path)
         if os.path.exists(file_path):
             os.remove(file_path)
+        logger.success("请求aip成功")
         return f'{cos_config["Prefix"]}{file_path}'
     else:
+        logger.error("请求aip失败")
         return None
